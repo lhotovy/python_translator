7dd3cdb5aa06cfe7adc2013c7901ba1b58be34b8
lhotovy <lhotovy@centrum.cz>
2023-04-25 15:16:00 +0200

    Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import deepl
+
+DEEPL_API_KEY = "e4219fbb-19e6-f0d9-f6d5-1dab58ad3b2f:fx"
+DEEPL_ENDPOINT = " https://api-free.deepl.com"
+
+translator = deepl.Translator(DEEPL_API_KEY)
+
+user_text = str(input("What do you want to translate? ").encode("utf-8"))
+target_lang = input('Select target language: ')
+
+
+result = translator.translate_text(user_text, target_lang=target_lang)
+print(result)
+print(result.detected_source_lang)
