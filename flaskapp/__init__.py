ab10d2920b72636654d2b74fa314e5c9f778bba9
Lukas Hotovy <112489845+lhotovy@users.noreply.github.com>
2023-05-21 07:07:04 +0200

    Hide secret key

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 # create and configure the app
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_mapping(
-SECRET_KEY="gd6df5g4fhfg64h6fggh46gh",
+SECRET_KEY="Deepl secret key goes here",
 )
 
 from flaskapp import routes
